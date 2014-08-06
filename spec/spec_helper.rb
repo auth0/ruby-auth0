fe2003ff9d63d77bfec268cf9ef9584106f2dc9b
Patrik Ragnarsson <patrik@starkast.net>
2014-08-06 15:31:22 +0200

    Fix rspec deprecation warning
        RSpec::Core::Configuration#color_enabled= is deprecated. Use
        RSpec::Core::Configuration#color= instead.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,6 @@ RSpec.configure do |config|
   config.include	WebMock::API
   config.include	Rack::Test::Methods
   config.extend		OmniAuth::Test::StrategyMacros, :type => :strategy
-  config.color_enabled = true
+  config.color = true
   config.formatter = 'documentation'
 end
