0b43009c041d2899a1dd32c99806d4424abe3103
Martin Gontovnikas <martin@gon.to>
2014-10-28 11:35:00 -0300

    Added commented how to change redirect

--- examples/ruby-on-rails-webapp/config/application.rb
+++ examples/ruby-on-rails-webapp/config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module RubyOnRailsWebapp
   class Application < Rails::Application
+
+
+    # Set this if you want to get the error_description
+    # OmniAuth.config.on_failure = Proc.new { |env|
+    #   message_key = env['omniauth.error.type']
+    #   error_description = Rack::Utils.escape(env['omniauth.error'].error_reason)
+    #   new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{message_key}&error_description=#{error_description}"
+    #   Rack::Response.new(['302 Moved'], 302, 'Location' => new_path).finish
+    # }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
