e498448dd0965956f29697e7aa6c670f83f6cd31
Sandrino Di Mattia <contact@sandrino.be>
2015-02-19 16:09:57 +0100

    Removed webapp example. Moved to omniauth-auth0

--- examples/ruby-on-rails-webapp/config/initializers/auth0.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(
-    :auth0,
-    ENV["AUTH0_CLIENT_ID"],
-    ENV["AUTH0_CLIENT_SECRET"],
-    ENV["AUTH0_DOMAIN"],
-    callback_path: "/auth/auth0/callback"
-  )
-end
