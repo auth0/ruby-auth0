4bd51edaa2a861bd972b478b28077f5879607ddc
Ethan Crawford <ethan_jc@hotmail.com>
2015-03-24 11:53:10 +1100

    fix token initialization to prevent nil token value

---
+++ spec/lib/auth0/mixins/initializer_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+class MockClass
+  attr_reader :token 
+  include Auth0::Mixins::Initializer
+  include HTTParty
+end
+
+describe Auth0::Mixins::Initializer do
+  let(:params) { {  } }
+  let(:instance) { DummyClassForProxy.include(described_class).new(params) }
+  context 'api v2' do
+    before do
+      params[:api_version] = 2
+    end
+    
+    it 'sets token when access_token is passed' do
+      params[:access_token] = '123'
+      
+      expect(instance.instance_variable_get('@token')).to eq('123')
+    end
+
+    it 'sets token when token is passed' do
+      params[:token] = '123'
+      
+      expect(instance.instance_variable_get('@token')).to eq('123')  
+    end
+  end
+end
