cb057e90c0ce35f2bfba414539bc58feff1909f6
Taylor Finnell <tmfinnell@gmail.com>
2015-05-29 19:21:49 -0400

    connection support in v2

---
+++ lib/auth0/api/v2/connections.rb
@@ -0,0 +1,45 @@
+module Auth0
+  module Api
+    module V2
+      module Connections
+        def connections(strategy: nil, fields: nil, include_fields: true)
+          request_params = {
+            strategy: strategy,
+            fields: fields,
+            include_fields: include_fields
+          }
+          path = "/api/v2/connections"
+          get(path, request_params)
+        end
+        alias :get_connections :connections
+
+        def create_connection(body)
+          path = "/api/v2/connections"
+          request_params = body
+          post(path, request_params )
+        end
+
+        def connection(connection_id, fields: nil, include_fields: true)
+          path = "/api/v2/connections/" + connection_id.to_s
+          request_params = {
+            fields:         fields,
+            include_fields: include_fields
+          }
+          get(path, request_params)
+        end
+
+        def delete_connection(connection_id)
+          raise Auth0::MissingConnectionId, "you must specify a connection id" if connection_id.to_s.empty?
+          path = "/api/v2/connections/#{connection_id}"
+          delete(path)
+        end
+
+        def update_connection(connection_id, body)
+          raise Auth0::MissingConnectionId, "you must specify a connection id" if connection_id.to_s.empty?
+          path = "/api/v2/connections/" + connection_id.to_s
+          patch(path, body)
+        end
+      end
+    end
+  end
+end
