4f0785d60f3062642ce6f3358d76c3dd8e87bce6
Hernan Zalazar <hernanzalazar@gmail.com>
2015-06-01 15:39:34 -0300

    Fix build

--- spec/integration/lib/auth0/api/v2/api_clients_spec.rb
+++ spec/integration/lib/auth0/api/v2/api_clients_spec.rb
@@ -7,9 +7,9 @@ describe Auth0::Api::V2::Clients do
 
   it { expect(client.clients).to_not be_empty }
 
-  it { expect(client.create_client(client_name, {custom_login_page_off: true})).to include("name" => client_name, "custom_login_page_off" => true) }
+  it { expect(client.create_client(client_name, {custom_login_page_on: false})).to include("name" => client_name, "custom_login_page_on" => false) }
 
-  it { expect(client.patch_client(existing_client["client_id"], {custom_login_page_off: true, sso: true})).to include("custom_login_page_off" => true, "sso" => true) }
+  it { expect(client.patch_client(existing_client["client_id"], {custom_login_page_on: false, sso: true})).to include("custom_login_page_on" => false, "sso" => true) }
 
   it { expect { client.delete_client(existing_client["client_id"]) }.to_not raise_error }
 
