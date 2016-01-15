ef331986c0e119ee87f9977cd05c918b3cda3470
Ignacio Jonas <ignacio.jonas@4-hills.com>
2016-01-15 15:36:45 -0300

    Update authentication endpoints

--- lib/auth0/api/authentication_endpoints.rb
+++ lib/auth0/api/authentication_endpoints.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/ModuleLength
 module Auth0
   module Api
     # {https://auth0.com/docs/auth-api}
@@ -6,13 +7,27 @@ module Auth0
       UP_AUTH = 'Username-Password-Authentication'
 
       # {https://auth0.com/docs/auth-api#!#post--oauth-access_token}
-      def obtain_access_token
+      def obtain_access_token(access_token = nil, connection = 'facebook', scope = 'openid')
+        if access_token
+          request_params = { client_id: @client_id, access_token: access_token, connection: connection, scope: scope }
+          post('/oauth/access_token', request_params)['access_token']
+        else
+          request_params = { client_id: @client_id, client_secret: @client_secret, grant_type: 'client_credentials' }
+          post('/oauth/token', request_params)['access_token']
+        end
+      end
+
+      # {https://auth0.com/docs/auth-api#!#post--delegation}
+      def refresh_delegation(refresh_token, target, scope = 'openid', api_type = 'app', extra_parameters = {})
         request_params = {
           client_id:      @client_id,
-          client_secret:  @client_secret,
-          grant_type:     'client_credentials'
-        }
-        post('/oauth/token', request_params)['access_token']
+          grant_type:     'urn:ietf:params:oauth:grant-type:jwt-bearer',
+          refresh_token:  refresh_token,
+          target:         target,
+          api_type:       api_type,
+          scope:          scope
+        }.merge(extra_parameters)
+        post('/delegation', request_params)
       end
 
       # {https://auth0.com/docs/auth-api#!#post--delegation}
@@ -59,7 +74,6 @@ module Auth0
       end
 
       # {https://auth0.com/docs/auth-api#!#post--oauth-ro}
-
       def login(username, password, scope = 'openid', id_token = nil, connection_name = UP_AUTH)
         request_params = {
           client_id:  @client_id,
@@ -69,6 +83,19 @@ module Auth0
           connection: connection_name,
           grant_type: 'password',
           id_token:   id_token
+        }
+        post('/oauth/ro', request_params)
+      end
+
+      # {https://auth0.com/docs/auth-api#!#post--ro_with_sms}
+      def phone_login(phone_number, code, scope = 'openid')
+        request_params = {
+          client_id:  @client_id,
+          username:   phone_number,
+          password:   code,
+          scope:      scope,
+          connection: 'sms',
+          grant_type: 'password'
         }
         post('/oauth/ro', request_params)
       end
@@ -95,10 +122,55 @@ module Auth0
         post('/dbconnections/change_password', request_params)
       end
 
+      # {https://auth0.com/docs/auth-api#!#post--with_email}
+      def start_passwordless_email_flow(email, send = 'link', auth_params = {})
+        request_params = {
+          client_id:  @client_id,
+          email:      email,
+          send: send,
+          auth_params: auth_params
+        }
+        post('/passwordless/start', request_params)
+      end
+
+      # {https://auth0.com/docs/auth-api#!#post--with_email}
+      def start_passwordless_sms_flow(phone_number)
+        request_params = {
+          client_id:  @client_id,
+          connection: 'sms',
+          phone_number: phone_number
+        }
+        post('/passwordless/start', request_params)
+      end
+
+      # {https://auth0.com/docs/auth-api#!#post--unlink}
+      def unlink_user(access_token, user_id)
+        request_params = {
+          access_token:  access_token,
+          user_id: user_id
+        }
+        post('/unlink', request_params)
+      end
+
       # {https://auth0.com/docs/auth-api#!#post--tokeninfo}
       def token_info(id_token)
         request_params = { id_token: id_token }
         post('/tokeninfo', request_params)
+      end
+
+      # {https://auth0.com/docs/auth-api#!#get--userinfo}
+      def user_info
+        get('/userinfo')
+      end
+
+      # {https://auth0.com/docs/auth-api#!#get--samlp--client_id-}
+      def saml_metadata(client_id)
+        get("/samlp/metadata/#{client_id}")
+      end
+
+      # {https://auth0.com/docs/auth-api#!#get--samlp--client_id-}
+      def wsfed_metadata
+        get('/wsfed/FederationMetadata/2007-06/FederationMetadata.xml')
       end
     end
   end
--- spec/lib/auth0/api/authentication_endpoints_spec.rb
+++ spec/lib/auth0/api/authentication_endpoints_spec.rb
@@ -18,6 +18,18 @@ describe Auth0::Api::AuthenticationEndpoints do
     end
   end
 
+  context '.obtain_access_token social' do
+    it { expect(@instance).to respond_to(:obtain_access_token) }
+    it "is expected to make post request to '/oauth/access_token'" do
+      allow(@instance).to receive(:post).with(
+        '/oauth/access_token', client_id: nil, access_token: 'access_token', connection: 'facebook', scope: 'openid')
+        .and_return('access_token' => 'AccessToken')
+      expect(@instance).to receive(:post).with(
+        '/oauth/access_token', client_id: nil, access_token: 'access_token', connection: 'facebook', scope: 'openid')
+      expect(@instance.obtain_access_token('access_token', 'facebook', 'openid')).to eql 'AccessToken'
+    end
+  end
+
   context '.delegation' do
     it { expect(@instance).to respond_to(:delegation) }
     it "is expected to make post request to '/delegation'" do
@@ -67,6 +79,19 @@ describe Auth0::Api::AuthenticationEndpoints do
     end
   end
 
+  context '.refresh_delegation' do
+    it { expect(@instance).to respond_to(:refresh_delegation) }
+    it "is expected to make post request to '/delegation'" do
+      expect(@instance).to receive(:post).with(
+        '/delegation',
+        client_id: nil,
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        refresh_token: '', target: '', api_type: '', scope: '',
+        additional_parameter: 'parameter')
+      @instance.refresh_delegation('', '', '', '', additional_parameter: 'parameter')
+    end
+  end
+
   context '.impersonate' do
     let(:user_id)         { 'some_user_id' }
     let(:app_client_id)   { 'some_app_client_id' }
@@ -96,6 +121,29 @@ describe Auth0::Api::AuthenticationEndpoints do
       @instance.login('test@test.com', 'password')
     end
   end
+
+  context '.phone_login' do
+    let(:phone_number) { Faker::PhoneNumber.cell_phone }
+    let(:code) { Faker::Number.number(10) }
+    it { expect(@instance).to respond_to(:phone_login) }
+    it 'is expected to make post to /oauth/ro' do
+      expect(@instance).to receive(:post).with(
+        '/oauth/ro',
+        client_id: nil, username: phone_number,
+        password: code, connection: 'sms',
+        scope: 'openid', grant_type: 'password')
+      @instance.phone_login(phone_number, code)
+    end
+  end
+
+  context '.unlink_user' do
+    it { expect(@instance).to respond_to(:phone_login) }
+    it 'is expected to make post to /unlink' do
+      expect(@instance).to receive(:post).with('/unlink', access_token: 'access-token', user_id: 'user-id')
+      @instance.unlink_user('access-token', 'user-id')
+    end
+  end
+
   context '.signup' do
     it { expect(@instance).to respond_to(:signup) }
     it 'is expected to make post to /dbconnections/signup' do
@@ -117,11 +165,65 @@ describe Auth0::Api::AuthenticationEndpoints do
     end
   end
 
+  context '.start_passwordless_email_flow' do
+    it { expect(@instance).to respond_to(:start_passwordless_email_flow) }
+    it 'is expected to make post to /passwordless/start' do
+      expect(@instance).to receive(:post).with(
+        '/passwordless/start',
+        client_id: nil,
+        email: 'test@test.com',
+        send: 'link',
+        auth_params: {
+          scope: 'scope',
+          protocol: 'protocol'
+        })
+      @instance.start_passwordless_email_flow('test@test.com', 'link', scope: 'scope', protocol: 'protocol')
+    end
+  end
+
+  context '.start_passwordless_sms_flow' do
+    let(:phone_number) { Faker::PhoneNumber.cell_phone }
+    it { expect(@instance).to respond_to(:start_passwordless_sms_flow) }
+    it 'is expected to make post to /passwordless/start' do
+      expect(@instance).to receive(:post).with(
+        '/passwordless/start',
+        client_id: nil,
+        connection: 'sms',
+        phone_number: phone_number)
+      @instance.start_passwordless_sms_flow(phone_number)
+    end
+  end
+
   context '.token_info' do
     it { expect(@instance).to respond_to(:token_info) }
     it 'is expected to make post to /tokeinfo' do
       expect(@instance).to receive(:post).with('/tokeninfo', id_token: 'SomerandomToken')
       @instance.token_info('SomerandomToken')
+    end
+  end
+
+  context '.user_info' do
+    it { expect(@instance).to respond_to(:user_info) }
+    it 'is expected to make post to /userinfo' do
+      expect(@instance).to receive(:get).with('/userinfo')
+      @instance.user_info
+    end
+  end
+
+  context '.saml_metadata' do
+    let(:client_id) { 'client-id' }
+    it { expect(@instance).to respond_to(:saml_metadata) }
+    it 'is expected to make post to /samlp/metadata/client-id' do
+      expect(@instance).to receive(:get).with("/samlp/metadata/#{client_id}")
+      @instance.saml_metadata(client_id)
+    end
+  end
+
+  context '.wsfed_metadata' do
+    it { expect(@instance).to respond_to(:wsfed_metadata) }
+    it 'is expected to make post to /wsfed/FederationMetadata/2007-06/FederationMetadata.xml' do
+      expect(@instance).to receive(:get).with('/wsfed/FederationMetadata/2007-06/FederationMetadata.xml')
+      @instance.wsfed_metadata
     end
   end
 end
