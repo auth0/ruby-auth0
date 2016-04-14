9a340754803dcf0e107c7111e07698f87846eba7
Lindsey Bieda <lbieda@articulate.com>
2016-04-14 12:52:10 -0400

    Updating specs

--- spec/lib/auth0/api/v2/logs_spec.rb
+++ spec/lib/auth0/api/v2/logs_spec.rb
@@ -10,7 +10,7 @@ describe Auth0::Api::V2::Logs do
     it { expect(@instance).to respond_to(:logs) }
     it 'is expected to call /api/v2/logs' do
       expect(@instance).to receive(:get).with(
-        'api/v2/logs',
+        '/api/v2/logs',
         q: nil,
         page: nil,
         per_page: nil,
@@ -27,7 +27,7 @@ describe Auth0::Api::V2::Logs do
   context '.user' do
     it { expect(@instance).to respond_to(:log) }
     it 'is expected to call get request to /api/v2/logs/LOG_ID' do
-      expect(@instance).to receive(:get).with('api/v2/logs/LOG_ID')
+      expect(@instance).to receive(:get).with('/api/v2/logs/LOG_ID')
       expect { @instance.log('LOG_ID') }.not_to raise_error
     end
   end
