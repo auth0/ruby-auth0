ee56670761e852d0ed1ed1bcfa51e9ff0e13f29f
Paul Millar <paul@digitaldawn.com.au>
2018-11-16 18:14:06 +1100

    Add check to ensure that the ttl_sec parameter passed through is an integer. Added specs to ensure this is the case.

--- spec/lib/auth0/api/v2/tickets_spec.rb
+++ spec/lib/auth0/api/v2/tickets_spec.rb
@@ -11,12 +11,23 @@ describe Auth0::Api::V2::Tickets do
                                                                                      result_url: nil, ttl_sec: nil)
       expect { @instance.post_email_verification('user_id') }.not_to raise_error
     end
+    it 'expect client to accept integer ttl_sec' do
+      expect(@instance).to receive(:post).with('/api/v2/tickets/email-verification', user_id: 'user_id',
+                                                                                     result_url: nil, ttl_sec: 300)
+      expect { @instance.post_email_verification('user_id', ttl_sec: 300) }.not_to raise_error
+    end
+    it 'expect client to return nil when calling with a non-integer ttl_sec' do
+      expect(@instance).to receive(:post).with('/api/v2/tickets/email-verification', user_id: 'user_id',
+                                                                                     result_url: nil, ttl_sec: nil)
+      expect { @instance.post_email_verification('user_id', ttl_sec: "noninteger") }.not_to raise_error
+    end
     it 'expect client to rasie error when calling with empty body' do
       expect { @instance.post_email_verification(nil) }.to raise_error(
         'Must supply a valid user id to post an email verification'
       )
     end
   end
+
   context '.post_password_change' do
     it { expect(@instance).to respond_to(:post_password_change) }
     it 'expect client to send post to /api/v2/tickets/password-change with body' do
