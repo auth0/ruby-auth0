ab7c5ad5e5d60a5442a9097b396385986a5e5aaf
Josh Cunningham <josh@joshcanhelp.com>
2019-06-26 15:33:13 -0700

    Minor refactor on validation methods

--- lib/auth0/mixins/validation.rb
+++ lib/auth0/mixins/validation.rb
@@ -4,16 +4,16 @@ module Auth0
     module Validation
 
       # Check a roles array
-      def validate_roles_array(roles)
-        raise Auth0::InvalidParameter, 'Must supply an array of role names' unless roles.kind_of?(Array)
-        raise Auth0::InvalidParameter, 'Must supply an array of role names' if roles.empty?
-        raise Auth0::InvalidParameter, 'All role names must be strings' unless roles.all? {|role| role.is_a? String}
+      def validate_strings_array(strings)
+        raise Auth0::InvalidParameter, 'Must supply an array of strings' unless strings.kind_of?(Array)
+        raise Auth0::MissingParameter, 'Must supply an array of strings' if strings.empty?
+        raise Auth0::InvalidParameter, 'All array elements must be strings' unless strings.all? {|str| str.is_a? String}
       end
 
       # Check a permissions array
       def validate_permissions_array(permissions)
         raise Auth0::InvalidParameter, 'Must supply an array of Permissions' unless permissions.kind_of?(Array)
-        raise Auth0::InvalidParameter, 'Must supply an array of Permissions' if permissions.empty?
+        raise Auth0::MissingParameter, 'Must supply an array of Permissions' if permissions.empty?
         raise Auth0::InvalidParameter, 'All array elements must be Permissions' unless permissions.all? do |permission|
           permission.kind_of? Permission
         end
