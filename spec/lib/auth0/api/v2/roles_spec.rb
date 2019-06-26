8159f1b2176af37dc09b123fcb0d7c92b9f7025e
Josh Cunningham <josh@joshcanhelp.com>
2019-06-26 14:11:38 -0700

    More failing tests for new roles endpoints

--- spec/lib/auth0/api/v2/roles_spec.rb
+++ spec/lib/auth0/api/v2/roles_spec.rb
@@ -14,6 +14,28 @@ describe Auth0::Api::V2::Roles do
     it 'is expected to have a roles alias' do
       expect(@instance).to respond_to(:roles)
     end
+
+    it 'is expected to get roles with default parameters' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/roles',
+        per_page: nil,
+        page: nil,
+        include_totals: nil
+      )
+      expect { @instance.get_roles }.not_to raise_error
+    end
+
+    it 'is expected to get roles with custom parameters' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/roles',
+        per_page: 10,
+        page: 3,
+        include_totals: true
+      )
+      expect do
+        @instance.get_roles(per_page: 10, page: 3, include_totals: true)
+      end.not_to raise_error
+    end
   end
 
   context '.get_role' do
@@ -31,6 +53,11 @@ describe Auth0::Api::V2::Roles do
         @instance.get_role('')
       end.to raise_exception Auth0::MissingParameter
     end
+
+    it 'is expected to get a role by ID' do
+      expect(@instance).to receive(:get).with('/api/v2/roles/ROLE_ID')
+      expect { @instance.get_role('ROLE_ID') }.not_to raise_error
+    end
   end
 
   context '.create_role' do
@@ -43,6 +70,20 @@ describe Auth0::Api::V2::Roles do
       expect do
         @instance.create_role('')
       end.to raise_exception Auth0::MissingParameter
+    end
+
+    it 'is expected to post a new role' do
+      expect(@instance).to receive(:post).with(
+        '/api/v2/roles',
+        name: 'ROLE_NAME',
+        description: 'ROLE_DESCRIPTION'
+      )
+      expect do
+        @instance.create_role(
+          'ROLE_NAME',
+          description: 'ROLE_DESCRIPTION'
+        )
+      end.not_to raise_error
     end
   end
 
@@ -57,6 +98,21 @@ describe Auth0::Api::V2::Roles do
         @instance.update_role('')
       end.to raise_exception Auth0::MissingParameter
     end
+
+    it 'is expected to post an updated role' do
+      expect(@instance).to receive(:patch).with(
+        '/api/v2/roles/ROLE_ID',
+        name: 'ROLE_NAME',
+        description: 'ROLE_DESCRIPTION'
+      )
+      expect do
+        @instance.update_role(
+          'ROLE_ID',
+          name: 'ROLE_NAME',
+          description: 'ROLE_DESCRIPTION'
+        )
+      end.not_to raise_error
+    end
   end
 
   context '.delete_role' do
@@ -69,6 +125,11 @@ describe Auth0::Api::V2::Roles do
       expect do
         @instance.delete_role('')
       end.to raise_exception Auth0::MissingParameter
+    end
+
+    it 'is expected to delete a role' do
+      expect(@instance).to receive(:delete).with('/api/v2/roles/ROLE_ID')
+      expect { @instance.delete_role('ROLE_ID') }.not_to raise_error
     end
   end
 
@@ -83,6 +144,28 @@ describe Auth0::Api::V2::Roles do
         @instance.get_users('')
       end.to raise_exception Auth0::MissingParameter
     end
+
+    it 'is expected to get users for a role with default parameters' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/roles/ROLE_ID/users',
+        per_page: nil,
+        page: nil,
+        include_totals: nil
+      )
+      expect { @instance.get_users('ROLE_ID') }.not_to raise_error
+    end
+
+    it 'is expected to get users for a role with custom parameters' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/roles/ROLE_ID/users',
+        per_page: 30,
+        page: 4,
+        include_totals: true
+      )
+      expect do
+        @instance.get_users('ROLE_ID', per_page: 30, page: 4, include_totals: true)
+      end.not_to raise_error
+    end
   end
 
   context '.add_users' do
@@ -93,8 +176,15 @@ describe Auth0::Api::V2::Roles do
     it 'is expected to raise an exception if an empty role ID is passed' do
       expect(@instance).not_to receive(:post)
       expect do
-        @instance.add_users('')
+        @instance.add_users('', [])
       end.to raise_exception Auth0::MissingParameter
+    end
+
+    it 'is expected to raise an exception if an array is not passed for users' do
+      expect(@instance).not_to receive(:post)
+      expect do
+        @instance.add_users('ROLE_ID', 'USERS')
+      end.to raise_exception Auth0::InvalidParameter
     end
 
     it 'is expected to raise an exception if no user IDs are passed' do
@@ -102,6 +192,16 @@ describe Auth0::Api::V2::Roles do
       expect do
         @instance.add_users('ROLE_ID', [])
       end.to raise_exception Auth0::MissingParameter
+    end
+
+    it 'is expected to add users to a role' do
+      expect(@instance).to receive(:post).with(
+        '/api/v2/roles/ROLE_ID/users',
+        users: %w[test|user-01 test|user-02]
+      )
+      expect do
+        @instance.add_users('ROLE_ID', %w[test|user-01 test|user-02])
+      end.not_to raise_error
     end
   end
 
@@ -115,6 +215,28 @@ describe Auth0::Api::V2::Roles do
       expect do
         @instance.get_permissions('')
       end.to raise_exception Auth0::MissingParameter
+    end
+
+    it 'is expected to get roles with default parameters' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/roles/ROLE_ID/permissions',
+        per_page: nil,
+        page: nil,
+        include_totals: nil
+      )
+      expect { @instance.get_permissions('ROLE_ID') }.not_to raise_error
+    end
+
+    it 'is expected to get roles with custom parameters' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/rolesROLE_ID/permissions',
+        per_page: 15,
+        page: 5,
+        include_totals: true
+      )
+      expect do
+        @instance.get_permissions('ROLE_ID', per_page: 15, page: 5, include_totals: true)
+      end.not_to raise_error
     end
   end
 
@@ -133,22 +255,47 @@ describe Auth0::Api::V2::Roles do
     it 'is expected to raise an exception if an array is not passed for permissions' do
       expect(@instance).not_to receive(:post)
       expect do
-        @instance.add_permissions('USER_ID', 'PERMISSIONS')
+        @instance.add_permissions('ROLE_ID', 'PERMISSIONS')
       end.to raise_exception Auth0::InvalidParameter
     end
 
     it 'is expected to raise an exception if an empty permissions array is passed' do
       expect(@instance).not_to receive(:post)
       expect do
-        @instance.add_permissions('USER_ID', [])
+        @instance.add_permissions('ROLE_ID', [])
       end.to raise_exception Auth0::MissingParameter
     end
 
     it 'is expected to raise an exception if the permissions array does not contain Permission structs' do
       expect(@instance).not_to receive(:post)
       expect do
-        @instance.add_permissions('USER_ID', [1, 2, 3])
+        @instance.add_permissions('ROLE_ID', [1, 2, 3])
       end.to raise_exception Auth0::InvalidParameter
+    end
+
+    it 'is expected to add permissions to a role' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/roles/ROLE_ID/permissions',
+        permissions: [
+          {
+            permission_name: 'permission-name-1',
+            resource_server_identifier: 'server-id-1'
+          },
+          {
+            permission_name: 'permission-name-2',
+            resource_server_identifier: 'server-id-2'
+          }
+        ]
+      )
+      expect do
+        @instance.add_permissions(
+          'ROLE_ID',
+          [
+            Permission.new('permission-name-1', 'server-id-1'),
+            Permission.new('permission-name-2', 'server-id-2')
+          ]
+        )
+      end.not_to raise_error
     end
   end
 
@@ -167,22 +314,47 @@ describe Auth0::Api::V2::Roles do
     it 'is expected to raise an exception if an array is not passed for permissions' do
       expect(@instance).not_to receive(:post)
       expect do
-        @instance.remove_permissions('USER_ID', 'PERMISSIONS')
+        @instance.remove_permissions('ROLE_ID', 'PERMISSIONS')
       end.to raise_exception Auth0::InvalidParameter
     end
 
     it 'is expected to raise an exception if an empty permissions array is passed' do
       expect(@instance).not_to receive(:delete)
       expect do
-        @instance.remove_permissions('USER_ID', [])
+        @instance.remove_permissions('ROLE_ID', [])
       end.to raise_exception Auth0::MissingParameter
     end
 
     it 'is expected to raise an exception if the permissions array does not contain Permission structs' do
       expect(@instance).not_to receive(:post)
       expect do
-        @instance.add_permissions('USER_ID', [1, 2, 3])
+        @instance.remove_permissions('ROLE_ID', [1, 2, 3])
       end.to raise_exception Auth0::InvalidParameter
+    end
+
+    it 'is expected to remove permissions from a role' do
+      expect(@instance).to receive(:delete).with(
+        '/api/v2/roles/ROLE_ID/permissions',
+        permissions: [
+          {
+            permission_name: 'permission-name-3',
+            resource_server_identifier: 'server-id-3'
+          },
+          {
+            permission_name: 'permission-name-4',
+            resource_server_identifier: 'server-id-4'
+          }
+        ]
+      )
+      expect do
+        @instance.remove_permissions(
+          'ROLE_ID',
+          [
+            Permission.new('permission-name-3', 'server-id-3'),
+            Permission.new('permission-name-4', 'server-id-4')
+          ]
+        )
+      end.not_to raise_error
     end
   end
 end
