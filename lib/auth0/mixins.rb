a1f729f9d5edd715241629b538a4c198eca7fc2a
David <david.patrick@auth0.com>
2020-10-20 12:52:22 -0700

    Remove v1 Endpoints

--- lib/auth0/mixins.rb
+++ lib/auth0/mixins.rb
@@ -11,7 +11,6 @@ require 'auth0/mixins/permission_struct'
 require 'auth0/mixins/validation'
 
 require 'auth0/api/authentication_endpoints'
-require 'auth0/api/v1'
 require 'auth0/api/v2'
 
 module Auth0
--- spec/support/credentials.rb
+++ spec/support/credentials.rb
@@ -1,24 +1,5 @@
 module Credentials
   module_function
-
-  def v1_creds
-    {
-      client_id: ENV['CLIENT_ID'],
-      client_secret: ENV['CLIENT_SECRET'],
-      domain: ENV['DOMAIN'],
-      api_version: 1
-    }
-  end
-
-  def v1_global_creds
-    {
-      client_id: ENV['GLOBAL_CLIENT_ID'],
-      client_secret: ENV['GLOBAL_CLIENT_SECRET'],
-      domain: ENV['DOMAIN'],
-      api_version: 1
-    }
-  end
-
   def v2_creds
     {
       domain: ENV.fetch( 'DOMAIN', 'DOMAIN' ),
