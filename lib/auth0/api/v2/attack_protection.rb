da202746d8a4005f512f93df8c875a5ae48e383c
Steve Hobbs <steve.hobbs@hey.com>
2022-02-16 14:17:24 +0000

    Test 'get breached-password-detection' endpoint

--- lib/auth0/api/v2/attack_protection.rb
+++ lib/auth0/api/v2/attack_protection.rb
@@ -8,16 +8,16 @@ module Auth0
           get(breached_password_settings_path)
         end
         alias get_breached_password_detection_settings breached_password_detection
-      end
-
-      private
-      
-      def attack_protection_path
-        @attack_protection_path ||= '/api/v2/attack_protection'
-      end
-
-      def breached_password_settings_path
-        "#{@attack_protection_path}/breached-password-detection"
+        
+        private
+        
+        def attack_protection_path
+          @attack_protection_path ||= '/api/v2/attack-protection'
+        end
+        
+        def breached_password_settings_path
+          "#{attack_protection_path}/breached-password-detection"
+        end
       end
     end
   end
--- spec/lib/auth0/api/v2/attack_protection_spec.rb
+++ spec/lib/auth0/api/v2/attack_protection_spec.rb
@@ -11,5 +11,17 @@ describe Auth0::Api::V2::AttackProtection do
     it 'is expected to respond to a breached_password_detection method' do
       expect(@instance).to respond_to(:breached_password_detection)
     end
+
+    it 'is expected to respond to get_breached_password_detection_settings' do
+      expect(@instance).to respond_to(:get_breached_password_detection_settings)
+    end
+
+    it 'is expected to get /api/v2/attack-protection/breached-password' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/attack-protection/breached-password-detection'
+      )
+
+      expect { @instance.breached_password_detection }.not_to raise_error
+    end
   end
 end
