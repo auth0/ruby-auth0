321066a393058937fc0abe266e5441aba5dfe7ab
shibayu36 <shibayu36@gmail.com>
2022-03-04 19:18:43 +0900

    Allow to pass nil to client_id arg of device_credentials (#321)
    Co-authored-by: Steve Hobbs <steve.hobbs@hey.com>

--- spec/lib/auth0/api/v2/device_credentials_spec.rb
+++ spec/lib/auth0/api/v2/device_credentials_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 describe Auth0::Api::V2::DeviceCredentials do
-  attr_reader :client_id
-
   before :all do
     dummy_instance = DummyClass.new
     dummy_instance.extend(Auth0::Api::V2::DeviceCredentials)
     @instance = dummy_instance
-    @client_id = Faker::Lorem.word
   end
   context '.device_credentials' do
     it { expect(@instance).to respond_to(:device_credentials) }
     it { expect(@instance).to respond_to(:list_device_credentials) }
-    it 'is expected to send get request to /api/v2/device-credentials' do
+    it 'is expected to send get request with no parameters to /api/v2/device-credentials' do
+      expect(@instance).to receive(:get).with(
+        '/api/v2/device-credentials', {
+        fields: nil,
+        include_fields: nil,
+        user_id: nil,
+        client_id: nil,
+        type: nil,
+        page: nil,
+        per_page: nil,
+        include_totals: nil
+      })
+      expect { @instance.device_credentials }.not_to raise_error
+    end
+    it 'is expected to send get request with client_id to /api/v2/device-credentials' do
+      client_id = Faker::Lorem.word
       expect(@instance).to receive(:get).with(
         '/api/v2/device-credentials', {
         fields: nil,
@@ -31,16 +43,16 @@ describe Auth0::Api::V2::DeviceCredentials do
         fields: 'name',
         include_fields: true,
         user_id: '1',
-        client_id: client_id,
+        client_id: 'client_id',
         type: 'rotating_refresh_token',
         page: 1,
         per_page: 10,
         include_totals: true
       })
-      expect { @instance.device_credentials(client_id, fields: 'name', include_fields: true, user_id: '1', type: 'rotating_refresh_token', page: 1, per_page: 10, include_totals: true) }.not_to raise_error
+      expect { @instance.device_credentials('client_id', fields: 'name', include_fields: true, user_id: '1', type: 'rotating_refresh_token', page: 1, per_page: 10, include_totals: true) }.not_to raise_error
     end
     it 'is expect to raise an error when type is not one of \'public_key\', \'refresh_token\', \'rotating_refresh_token\'' do
-      expect { @instance.device_credentials(client_id, type: 'invalid_type') }.to raise_error(
+      expect { @instance.device_credentials('client_id', type: 'invalid_type') }.to raise_error(
         'Type must be one of \'public_key\', \'refresh_token\', \'rotating_refresh_token\''
       )
     end
