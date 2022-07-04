88b3873af63ae1269f8176a279395b870be27631
Evan Sims <evan.sims@okta.com>
2022-07-04 18:00:12 -0500

    Update Codecov dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,8 +13,8 @@ require 'simplecov'
 SimpleCov.start
 
 if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 
 require 'dotenv'
