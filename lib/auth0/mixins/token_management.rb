17c9c55e7217c461e6847dce2de038ecdf34d5f9
Tristan Starck <tstarck@invoca.com>
2025-09-17 11:57:59 -0700

    Make get_token a public method on the Auth0::Client
    Currently if I want to generate an API token for an API using the Auth0::Client, I must do the following:
    ```ruby
    client = Auth0::Client.new(api_identifier: "my-api-identifier", **options)
    api_token = client.api_token("my-api-identifier")
    do_something(api_token) # Make a request to this API with the api token as the Authorization bearer.
    ```
    
    However `Auth0::Client.new` will actually already request this api token on initialize, however this token is not available via any public interface.
    
    This commit moves the `Auth0::Client#get_token` to a public method (formerly private).
    
    Now when in need of any API token for an API, we can do the following:
    ```ruby
    client = Auth0::Client.new(api_identifier: "my-api-identifier", **options)
    api_token = client.get_token # No need for redundant request to `/oauth/token`.
    do_something(api_token) # i.e make an http request to this API using the API token as the Authorization bearer.
    ```

--- lib/auth0/mixins/token_management.rb
+++ lib/auth0/mixins/token_management.rb
@@ -1,22 +1,14 @@
 module Auth0
   module Mixins
     module TokenManagement
-      
-      private
 
-      def initialize_token(options)
-        @token = options[:access_token] || options[:token]
-        # default expiry to an hour if a token was given but no expires_at
-        @token_expires_at = @token ? options[:token_expires_at] || Time.now.to_i + 3600 : nil 
-
-        @audience = options[:api_identifier] || "https://#{@domain}/api/v2/"
-        get_token() if @token.nil?
-      end
-
+      # Get the Client's api token (or generate a new one if it has expired).
+      #
+      # @return [String] the api token
       def get_token
         # pp @token_expires_at
         has_expired = @token && @token_expires_at ? @token_expires_at < (Time.now.to_i + 10) : false
-        
+
         if (@token.nil? || has_expired) && @client_id && (@client_secret || @client_assertion_signing_key)
           response = api_token(audience: @audience)
           @token = response.token
@@ -26,6 +18,17 @@ module Auth0
         else
           @token
         end
+      end
+
+      private
+
+      def initialize_token(options)
+        @token = options[:access_token] || options[:token]
+        # default expiry to an hour if a token was given but no expires_at
+        @token_expires_at = @token ? options[:token_expires_at] || Time.now.to_i + 3600 : nil
+
+        @audience = options[:api_identifier] || "https://#{@domain}/api/v2/"
+        get_token() if @token.nil?
       end
     end
   end
